0a34d001f43e39784dccc8cb89b2d0bb385642f2
aggre <hiroyuki.aggre@gmail.com>
2021-03-02 17:00:55 +0900

    simplified

--- src/fetch-github-repositories.ts
+++ src/fetch-github-repositories.ts
@@ -7,16 +7,11 @@ const URL =
 const fetchGithubRepositories = always(
 	(async (fetcher) =>
 		fetcher(`${URL}?${Math.random()}`).then(
-			(r) => (r as unknown) as ReadonlyMap<string, boolean>
+			(r) => (r as unknown) as Record<string, boolean>
 		))(bent('json'))
 )
 
 export const isAuthenticated = async (repository: string): Promise<boolean> => {
 	const res = await fetchGithubRepositories()
-	const record = Object.entries(res).filter(
-		([repositoryName, isAuthenticateValue]) => {
-			return repositoryName === repository && Boolean(isAuthenticateValue)
-		}
-	)
-	return record.length > 0
+	return Boolean(res[repository])
 }
