7008ab4b0c6e72f37ec7232aa0ee730d40aef6cb
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2022-01-11 10:46:19 +0900

    update address functions

--- src/addresses.test.ts
+++ src/addresses.test.ts
@@ -1,22 +1,18 @@
+/* eslint-disable functional/immutable-data */
+
 import test from 'ava'
 import { addresses } from './addresses'
 
 test('Returns mainnet address when the passed network is mainnet', async (t) => {
+	process.env[`KHAOS_MAINNET_GITHUB_MARKET_L2`] = '0x1234567890hogehoge'
 	const res = await addresses({ network: 'mainnet' })
-	t.is(res, '0x3cB902625a2B38929f807f9c841F7aecBbCe7702')
-})
-
-test('Returns ropsten address when the passed network is ropsten', async (t) => {
-	const res = await addresses({ network: 'ropsten' })
-	t.is(res, '0xE071bb5861e2352C89992799896D124F1bA5d599')
+	t.is(res, '0x1234567890hogehoge')
+	process.env[`KHAOS_MAINNET_GITHUB_MARKET_L2`] = undefined
 })
 
 test('Returns arbitrum rinkeby address when the passed network is arbitrum-rinkeby', async (t) => {
+	process.env[`KHAOS_ARBITRUM_RINKEBY_GITHUB_MARKET_L2`] = '0xhogehoge1234567890'
 	const res = await addresses({ network: 'arbitrum-rinkeby' })
-	t.is(res, '0x377B7d9C2DA6eD293EA62d2bCdA1cF54009751F8')
-})
-
-test('Returns empty string when the passed network is arbitrum-one', async (t) => {
-	const res = await addresses({ network: 'arbitrum-one' })
-	t.is(res, '0xE642B3a60F6Ef0e6dACF2388725eb487059E7739')
+	t.is(res, '0xhogehoge1234567890')
+	process.env[`KHAOS_ARBITRUM_RINKEBY_GITHUB_MARKET_L2`] = undefined
 })
