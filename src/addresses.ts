f1159f9f262ffad6e79a7975ef2083ab4dbee6f5
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2022-01-11 09:42:43 +0900

    update address function

--- src/addresses.ts
+++ src/addresses.ts
@@ -1,12 +1,10 @@
-import { FunctionAddresses } from '@devprotocol/khaos-core'
+import { FunctionAddresses, NetworkName } from '@devprotocol/khaos-core'
 
-export const addresses: FunctionAddresses = async ({ network: net }) =>
-	net === 'mainnet'
-		? '0x3cB902625a2B38929f807f9c841F7aecBbCe7702'
-		: net === 'ropsten'
-		? '0xE071bb5861e2352C89992799896D124F1bA5d599'
-		: net === 'arbitrum-rinkeby'
-		? '0x377B7d9C2DA6eD293EA62d2bCdA1cF54009751F8'
-		: net === 'arbitrum-one'
-		? '0xE642B3a60F6Ef0e6dACF2388725eb487059E7739'
-		: undefined
+export const addresses: FunctionAddresses = async ({ network: net }) => {
+	const keyName = getKeyNameFromNetworkName(net)
+	return process.env[`KHAOS_${keyName}_GITHUB_MARKET_L2`]
+}
+
+const getKeyNameFromNetworkName = (network: NetworkName): string => {
+	return network.toUpperCase().replace('-', '_')
+}
